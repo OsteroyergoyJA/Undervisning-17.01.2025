d86b71a76454436174c01a47dd6945afd3831665
OsteroyergoyJA <oosteroyergoy@gmail.com>
2025-01-17 12:06:27 +0100

    Undervisninh 17.01.2025

--- script.js
+++ script.js
@@ -17,6 +17,48 @@ async function displayPokemonData() {
   console.log(pokemonData.sprites.back_shiny);
   console.log(pokemonData.stats[1].base_stat); // attack number
   console.log(pokemonData.stats[1].stat.name); // attack name
+
+  for (let i = 0; i < pokemonData.stats.length; i++) {
+    const pokemonStats = pokemonData.stats[i];
+    console.log(pokemonStats);
+    console.log(pokemonStats.base_stat); // Attack number
+    console.log(pokemonStats.stat.name); // attack name
+  }
+
+  const pokemonCard = `
+
+  <div>
+      <h2>${pokemonData.id}. ${pokemonData.name}</h2>
+      <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
+      <ul>
+        ${pokemonData.stats.map((stat) => `
+          <li>${stat.base_stat} ${stat.stat.name}</li>
+        `).join('')}
+      </ul>
+    </div>
+  `;
+
+
+  console.log(pokemonCard);
+  
+  // const pokeDex = document.getElementById("pokedex");
+  /*<div>
+  <h2>
+  ${pokemonData.id}. ${pokemonData.name}
+  </h2>
+  <img src="${pokemonData.sprites.front_default}"/s>
+  <ul>
+  `;
+  <li>${pokemonStats.base_stats} ${pokemonStats.stat.name}</li>
+  <li>${pokemonStats.base_stats} ${pokemonStats.stat.name}</li>
+  <li>${pokemonStats.base_stats} ${pokemonStats.stat.name}</li>
+  <li>${pokemonStats.base_stats} ${pokemonStats.stat.name}</li>
+  <li>${pokemonStats.base_stats} ${pokemonStats.stat.name}</li>
+  </ul>
+  </div>
+  // pokeDex.appendChild(pokemonCard.innerHTML);*/
+
+  document.getElementById('pokedex').innerHTML = pokemonCard;
 }
 
 displayPokemonData();
